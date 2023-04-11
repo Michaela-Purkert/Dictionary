164be4cdfc77097526fa1f132b035a3bbe1df561
Michaela-Purkert <129502870+Michaela-Purkert@users.noreply.github.com>
2023-04-11 10:06:29 +0200

    add library project (#2)
    Co-authored-by: Chlupus <purkertovam@gmail.com>

---
+++ CheckLibrary/Check.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+
+namespace CheckLibrary
+{
+    public class Check
+    {
+        public static int PositiveIntCheck()
+        {
+            int option;
+            bool checkInt = false;
+            do
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out option))
+                {
+                    if (option > 0)
+                    {
+                        checkInt = true;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("The inserted integer must be positive\n");
+                        Console.WriteLine("Try it again:");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input must be a positive integer\n");
+                    Console.WriteLine("Try it again:");
+                }
+            } while (checkInt == false);
+            return option;
+        }
+
+        public static string NotEmptyString()
+        {
+            bool checkString = false;
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("You must write a text");
+                    Console.WriteLine("Try it again:");
+                }
+                else
+                    checkString = true;
+            } while (checkString == false);
+            return input;
+        }
+    }
+}
